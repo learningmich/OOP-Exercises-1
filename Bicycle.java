ef481c43fdf0417a75da1954877be2cea5395ee9
Michelle D. Almen <117343266+learningmich@users.noreply.github.com>
2023-01-07 17:27:05 +0800

    Update Bicycle.java

--- Bicycle.java
+++ Bicycle.java
@@ -1,3 +1,18 @@
 public class Bicycle
 {
+	private String id;
+	public String ownerName;
+
+	public Bicycle(){
+		ownerName = "Unknown";
+	}
+	public String getOwnerName(){
+		return ownerName;
+	}
+	public void setOwnerName(String name){
+		ownerName = name;
+	}
+	public Bicycle(String tagNo){
+		id = tagNo;
+	}
 }
